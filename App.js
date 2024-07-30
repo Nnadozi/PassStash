2d8a23cee3b81f23f2b507c52f6359c457ef1cc6
Zixon77 <chikaosro@gmail.com>
2024-07-29 20:10:21 -0400

    App is Ready!

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useRef, useEffect} from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './screens/Home';
 import AddItem from './screens/AddItem';
 import ViewItem from './screens/ViewItem';
 import { useNavigation, DarkTheme,DefaultTheme } from '@react-navigation/native';
-import { Button,useColorScheme } from 'react-native';
+import { Animated, Button,useColorScheme } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
@@ -22,10 +22,23 @@ function AddItemIcon() {
 
 
 export default function App() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
   const scheme = useColorScheme();
+
+  useEffect(() => {
+    const loadResources = async () => {
+      Animated.timing(fadeAnim, {
+        toValue:1,
+        duration:1000,
+        useNativeDriver:true,
+      }).start();
+    };
+    loadResources();
+  }, []);
   return (
     <>
     <StatusBar />
+    <Animated.View style = {{flex:1, opacity:fadeAnim}}>
     <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack.Navigator
         screenOptions={{
@@ -57,6 +70,7 @@ export default function App() {
           }} />
       </Stack.Navigator>
     </NavigationContainer>
+    </Animated.View>
     </>
   );
 }
