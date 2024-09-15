e435763988ddd7a595b622e6c6b3966907787c9d
Zixon77 <chikaosro@gmail.com>
2024-09-15 01:30:49 -0400

    Cooked

--- app.config.js
+++ app.config.js
@@ -1,5 +1,7 @@
 export default ({ config }) => ({
   ...config,
+  version: "1.0.0",  
+  runtimeVersion: "2.0.0",  
   plugins: [
     ...(config.plugins || []),
     ['react-native-google-mobile-ads', { androidAppId: process.env.EXPO_PUBLIC_APP_ID }],
