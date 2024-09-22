d407c2f67e9ba38dbb42f47882894131c851ca11
Zixon77 <chikaosro@gmail.com>
2024-09-22 15:17:53 -0400

    Finally fixed

--- app.config.js
+++
@@ -1,9 +0,0 @@
-export default ({ config }) => ({
-  ...config,
-  version: "1.0.0",  
-  runtimeVersion: "2.0.0",  
-  plugins: [
-    ...(config.plugins || []),
-    ['react-native-google-mobile-ads', { androidAppId: process.env.EXPO_PUBLIC_APP_ID }],
-  ],
-});
